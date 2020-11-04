be7151f598e46290ce7d1cb650e682ba69be8691
LEONARDO PEREIRA ALVES <004094793@itaud.des.ihf>
2020-11-04 10:00:58 -0300

    leo03

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const URL="https://java-api-spring-bot.herokuapp.com/validacpf?cpf="
+const URL="https://leperal-cpf-backspringboot.herokuapp.com/validarcpf?cpf="
 //const URL="https://leperalvalidar-cpf-front-js-va.herokuapp.com/validarcpf?cpf="
 function validar(){
 
